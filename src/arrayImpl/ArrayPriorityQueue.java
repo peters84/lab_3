2849dc8e3958c1c4d6eb94a354f8660db4bc9a7e
peters <peters@192.168.41.195>
2016-08-03 15:42:53 -0400

    Advance array implementation

--- src/arrayImpl/ArrayPriorityQueue.java
+++ src/arrayImpl/ArrayPriorityQueue.java
@@ -7,14 +7,35 @@ import lab_3.PriorityQueueInterface;
 
 /**
  * @author Peter Socha
- *
+ * 
+ * 
  */
-public class ArrayPriorityQueue<T extends Comparable<? super T>> implements PriorityQueueInterface <T> {
 
+
+public class ArrayPriorityQueue<T extends Comparable<? super T>> implements PriorityQueueInterface <T> {
+	private T[] theArray;
+	private static final int DEFAULT_SIZE = 10;
+	private int lastIndex;
+
+
+	public ArrayPriorityQueue(int Capacity) {
+		@SuppressWarnings("unchecked")
+		T[] anArray = (T[]) new Object[Capacity];
+		theArray = anArray;
+	}
+
+	public ArrayPriorityQueue() {
+		// TODO Auto-generated constructor stub
+		this(DEFAULT_SIZE);
+	}
 	@Override
 	public void add(T newEntry) {
 		// TODO Auto-generated method stub
+		for (T genObj:theArray){
+			if (newEntry.compareTo(genObj)>0){
 
+			}
+		}
 	}
 
 	@Override
